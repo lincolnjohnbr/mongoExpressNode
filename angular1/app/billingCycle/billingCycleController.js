3baa2f283d8e05f283d467e8c9a1d5049980ac28
lincolnjohn <lincolnjohn@gmail.com>
2018-12-27 14:53:47 -0300

    Factory Msg e Lista Pagamentos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,19 +1,32 @@
 (function () {
   angular.module('primeiraApp').controller('BillingCycleCtrl',[
     '$http',
+    'msgs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http) {
+  function BillingCycleController($http, msgs) {
     const vm = this
+    const url = 'http://localhost:3003/api/billingCycles'
 
-    vm.create = function () {
-      const url = 'http://localhost:3003/api/billingCycles'
-      $http.post(url,vm.billingCycle).then(function (response) {
+    vm.refresh = function () {
+      $http.get(url).then(function (response) {
         vm.billingCycle = {}
-        console.log('Sucesso!!');
+        vm.billingCycles = response.data
       })
     }
+
+    vm.create = function () {
+      $http.post(url,vm.billingCycle).then(function (response) {
+        vm.refresh()
+        console.log('Sucesso!!');
+        msgs.addSuccess('Operação realizada com Sucesso!!')
+      }).catch(function (resp) {
+        msgs.addError(resp.data.errors)
+      })
+    }
+
+    vm.refresh()
 
   }
 })()
